7dbac46fc57456c31b603f51cb41f3a49d7c886e
priyanka kirane <priyankakiranesd@gmail.com>
2021-02-02 16:38:15 +0530

    updated yml file

--- cypress/integration/DEMO/DummyTest.js
+++ cypress/integration/DEMO/DummyTest.js
@@ -1,12 +1,12 @@
 describe('handling frames',function() {
     it('Test1',function() {
-        crypto.log("test case passed")
+        cy.log("test case passed")
         })
 
   
 
     it('Test2',function() {
-        crypto.log("test case passed")
+        cy.log("test case passed")
         })
 
         
